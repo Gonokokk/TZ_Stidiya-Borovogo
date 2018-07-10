02ee7b02e3c34e77eda07946117e6d28f6c13457
Gonokokk <piston16011985@gmail.com>
2018-07-10 17:23:31 +0300

    add slider

---
+++ src/js/slider.js
@@ -0,0 +1,13 @@
+var slideIndex = 0;
+carousel();
+
+function carousel() {
+	var slides = document.getElementsByClassName("slide");
+    for (var i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none"; 
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1} 
+    slides[slideIndex-1].style.display = "block"; 
+    setTimeout(carousel, 7500); 
+}
